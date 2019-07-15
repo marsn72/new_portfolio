4f2a5bde7ea11a5946023b25d838ab822c7ad270
Sergey <maraev@mail.ru>
2019-07-15 22:22:03 +0300

    start and make gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+
+gulp.task('pug', ()=>{
+  gulp.src('./src/**/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('watch', ()=>{
+  gulp.watch('./src/**/*.pug', ['pug'])
+});
